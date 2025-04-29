a495e5b4ec452e2b2431d0e5a8a343021d5fcff2
subigya-js <developer.subigya@gmail.com>
2025-04-29 10:10:10 +0530

    initialize: Initialized the NextJS application

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Feel free to modify/remove this option
+
+  // Override the default webpack configuration
+  webpack: (config) => {
+    // See https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      sharp$: false,
+      "onnxruntime-node$": false,
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
